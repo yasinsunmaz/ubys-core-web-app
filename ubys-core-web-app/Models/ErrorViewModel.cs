300746f858c0d13cd799a924f1d2aa738a4563fd
yasin.sunmaz <yasin.sunmaz@ikcu.edu.tr>
2026-04-21 13:59:00 +0300

    Add project files.

---
+++ ubys-core-web-app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ubys_core_web_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
